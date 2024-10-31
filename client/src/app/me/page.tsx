9e3bb3d66a2511665bd5e0246f797ab6def3fa95
huynvn <Huy.Nguyen@navigosgroup.com>
2024-10-31 16:00:16 +0700

    25. Làm chức năng cập nhật Profile. Dùng router.refresh() để refresh route

--- client/src/app/me/page.tsx
+++ client/src/app/me/page.tsx
@@ -1,17 +1,21 @@
 import { cookies } from "next/headers";
 import Profile from "./profile";
 import accountApiRequest from "@/apiRequest/account";
+import ProfileForm from "./profile-form";
 
 export default async function MeProfile() {
   const cookiesStore = cookies();
   const sessionToken = cookiesStore.get("sessionToken");
+
+  // TODO: Vì dùng cookie nên api này không được cached trên server.
   const result = await accountApiRequest.me(sessionToken?.value ?? "");
 
   return (
     <div>
       <h1>Profile</h1>
-      <div>Xin chào: {result.payload.data.name}</div>
-      {/* <Profile /> */}
+      <h3>{result.payload.data.name}</h3>
+      {/* TODO: khi nào fetch thành công thì mới render ra ProfileForm nên profile props lúc nào cũng có data */}
+      <ProfileForm profile={result.payload.data} />
     </div>
   );
 }
---
+++ client/src/app/me/profile-form.tsx
@@ -0,0 +1,102 @@
+"use client";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { LoginBody, LoginBodyType } from "@/schemaValidations/auth.schema";
+import { useToast } from "@/components/ui/use-toast";
+import authApiRequest from "@/apiRequest/auth";
+import { useRouter } from "next/navigation";
+import { clientSessionToken } from "@/lib/http";
+import { handleErrorApi } from "@/lib/utils";
+import {
+  AccountResType,
+  UpdateMeBody,
+  UpdateMeBodyType,
+} from "@/schemaValidations/account.schema";
+import accountApiRequest from "@/apiRequest/account";
+
+type ProfileFormProps = {
+  profile: AccountResType["data"];
+};
+
+export default function ProfileForm({ profile }: ProfileFormProps) {
+  const [loading, setLoading] = useState(false);
+  const { toast } = useToast();
+  // const { setSessionToken } = useAppContext();
+  const router = useRouter();
+  const form = useForm<UpdateMeBodyType>({
+    resolver: zodResolver(UpdateMeBody),
+    defaultValues: {
+      name: profile.name,
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: UpdateMeBodyType) {
+    if (loading) return;
+    setLoading(true);
+    try {
+      const result = await accountApiRequest.updateMe(values);
+
+      toast({
+        description: result.payload.message,
+      });
+
+      // TODO: resfresh server component cha, gửi request lên react server để call lại API get và cập nhật lại.
+      router.refresh();
+    } catch (error: any) {
+      handleErrorApi({ error, setError: form.setError });
+    } finally {
+      setLoading(false);
+    }
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit, (error) => {
+          console.log(error);
+        })}
+        className='space-y-2 max-w-[600px] flex-shrink-0 w-full'
+        noValidate
+      >
+        <FormLabel>Email</FormLabel>
+        <FormControl>
+          <Input
+            placeholder='email'
+            type='email'
+            value={profile.email}
+            readOnly
+          />
+        </FormControl>
+        <FormMessage />
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Tên</FormLabel>
+              <FormControl>
+                <Input placeholder='tên' type='text' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit' className='!mt-8 w-full'>
+          Cập nhật
+        </Button>
+      </form>
+    </Form>
+  );
+}
