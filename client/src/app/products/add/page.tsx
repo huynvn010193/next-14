2f96be996701f7d0543eab4565cccaf3dcc2618c
huynvn <Huy.Nguyen@navigosgroup.com>
2024-10-31 23:10:04 +0700

    Pending 26.Upload image 7:02

---
+++ client/src/app/products/add/page.tsx
@@ -0,0 +1,12 @@
+import ProductAddForm from "./product-add-form";
+
+export interface ProducePageProps {}
+
+export default function ProducePageProps(props: ProducePageProps) {
+  return (
+    <div>
+      ProducePageProps
+      <ProductAddForm />
+    </div>
+  );
+}
---
+++ client/src/app/products/add/product-add-form.tsx
@@ -0,0 +1,129 @@
+"use client";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+import authApiRequest from "@/apiRequest/auth";
+import { useRouter } from "next/navigation";
+import { clientSessionToken } from "@/lib/http";
+import { handleErrorApi } from "@/lib/utils";
+import {
+  CreateProductBody,
+  CreateProductBodyType,
+} from "@/schemaValidations/product.schema";
+import productApiRequest from "@/apiRequest/product";
+import { Textarea } from "@/components/ui/textarea";
+
+interface ProductAddProps {}
+
+export default function ProductAddForm(props: ProductAddProps) {
+  const [loading, setLoading] = useState(false);
+  const { toast } = useToast();
+  // const { setSessionToken } = useAppContext();
+  const router = useRouter();
+  const form = useForm<CreateProductBodyType>({
+    resolver: zodResolver(CreateProductBody),
+    defaultValues: {
+      name: "",
+      price: 0,
+      description: "",
+      image: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: CreateProductBodyType) {
+    if (loading) return;
+    setLoading(true);
+    try {
+      const result = await productApiRequest.create(values);
+
+      toast({
+        description: result.payload.message,
+      });
+
+      router.push("/products");
+    } catch (error: any) {
+      handleErrorApi({ error, setError: form.setError });
+    } finally {
+      setLoading(false);
+    }
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit, (error) => {
+          console.log(error);
+        })}
+        className='space-y-2 max-w-[600px] flex-shrink-0 w-full'
+        noValidate
+      >
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Tên</FormLabel>
+              <FormControl>
+                <Input placeholder='Tên' type='text' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='price'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Giá</FormLabel>
+              <FormControl>
+                <Input placeholder='giá' type='number' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='description'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Mô tả</FormLabel>
+              <FormControl>
+                <Textarea placeholder='mô tả' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='image'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Mô tả</FormLabel>
+              <FormControl>
+                <Textarea placeholder='mô tả' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit' className='!mt-8 w-full'>
+          Đăng nhập
+        </Button>
+      </form>
+    </Form>
+  );
+}
