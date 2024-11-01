85c9fbf27a0e05bf75193d83994a0d36082f8fcb
huynvn <Huy.Nguyen@navigosgroup.com>
2024-11-01 23:35:21 +0700

    27.Render trang danh sách sản phẩm bằng ServerComponent

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,10 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.pexels.com",
-        port: "",
-        pathname: "/**",
+        protocol: "http",
+        hostname: "localhost",
+        port: "4000",
       },
     ],
   },
